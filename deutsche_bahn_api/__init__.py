af00de7e021baa4fa93a7704fbf81e08cacdb7d2
Tutorialwork <tutorialworktv@gmail.com>
2025-04-14 20:31:36 +0200

    Added init.py

---
+++ deutsche_bahn_api/__init__.py
@@ -0,0 +1,9 @@
+from .api_authentication import ApiAuthentication
+from .station_helper import StationHelper
+from .timetable_helper import TimetableHelper
+from .message import Message
+from .station import Station
+from .train import Train
+from .train_changes import TrainChanges
+
+__all__ = ["ApiAuthentication", "StationHelper", "TimetableHelper", "Message", "Station", "Train", "TrainChanges"]
